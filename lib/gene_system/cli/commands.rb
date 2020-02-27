4bd1236d8bc5acdd798abe23ce71d129282f1631
andrewbigger <abigger@zendesk.com>
2020-02-27 18:12:30 +1100

    Add install and remove commands and command tests

--- lib/gene_system/cli/commands.rb
+++ lib/gene_system/cli/commands.rb
@@ -2,47 +2,75 @@ module GeneSystem
   module CLI
     # CLI Actions
     module Commands
-      ##
-      # Creates a new, blank manifest with at the specified
-      # destination with the given name
-      #
-      # It is expected that the first argument provided to the
-      # command will be the name of the manifest. If this is not
-      # provided then a RuntimeError will be raised.
-      #
-      # @param dest [String]
-      # @param args [Array]
-      #
-      def self.new(dest, args = [])
-        manifest_name = args.shift
+      class <<self
+        ##
+        # Creates a new, blank manifest with at the specified
+        # destination with the given name
+        #
+        # It is expected that the first argument provided to the
+        # command will be the name of the manifest. If this is not
+        # provided then a RuntimeError will be raised.
+        #
+        # @param dest [String]
+        # @param args [Array]
+        #
+        def new(args = [])
+          manifest_name = args.shift
 
-        raise 'no manifest name provided' unless manifest_name
+          raise 'no manifest name provided' unless manifest_name
 
-        GeneSystem::Generators.render_empty_manifest(
-          manifest_name,
-          dest
-        )
-      end
-
-      ##
-      # Applies a manifest to the host system
-      #
-      # @param dest [String]
-      # @param args [Array]
-      #
-      def self.apply(dest, args = [])
-        manifest_file_name = args.shift
-
-        raise 'no manifest name provided' unless manifest_file_name
-
-        manifest_path = File.join(dest, manifest_file_name)
-
-        unless File.exist?(manifest_path)
-          raise "cannot find manifest at #{manifest_path}"
+          GeneSystem::Generators.render_empty_manifest(
+            manifest_name,
+            Dir.pwd
+          )
         end
 
-        manifest = GeneSystem::Manifest.new_from_file(manifest_path)
-        puts manifest
+        ##
+        # Applies install instructions from a manifest to the host system
+        #
+        # @param args [Array]
+        #
+        def install(args = [])
+          manifest = load_manifest(args)
+          platform = GeneSystem::Platform.new
+
+          manifest.steps.each do |step|
+            platform.execute_commands(step.install.cmd)
+          end
+
+          GeneSystem::CLI.print_message("\nmanifest successfully installed")
+        end
+
+        ##
+        # Applies remove instructions from a manifest to the host system
+        #
+        # @param args [Array]
+        #
+        def remove(args = [])
+          manifest = load_manifest(args)
+          platform = GeneSystem::Platform.new
+
+          manifest.steps.each do |step|
+            platform.execute_commands(step.remove.cmd)
+          end
+
+          GeneSystem::CLI.print_message("\nmanifest successfully removed")
+        end
+
+        private
+
+        def load_manifest(args)
+          manifest_rel = args.shift
+          raise 'no manifest path provided' unless manifest_rel
+
+          manifest_path = File.join(Dir.pwd, manifest_rel)
+
+          unless File.exist?(manifest_path)
+            raise "cannot find manifest at #{manifest_path}"
+          end
+
+          GeneSystem::Manifest.new_from_file(manifest_path)
+        end
       end
     end
   end
